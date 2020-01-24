18ca00aa625ff5c554cde84d6cdd9e4b6c1d9efe
dglasson18 <47996639+dglasson18@users.noreply.github.com>
2020-01-24 22:38:59 +1100

    Each sensor has individual page
    receiverModified.py updated - not yet tested - so that data received from each sensor is written to its own page, rather than having a single page for each type of sensor where data from all sensors of that type is displayed

--- receiverModified.py
+++ receiverModified.py
@@ -28,11 +28,17 @@ import serial
 # Set the GPIO naming convention
 GPIO.setmode(GPIO.BCM)
 
+htmlMenuText = '<!DOCTYPE html>\n<html>\n<body><h1><a href ="tankFile.html"><Tanks </a><a href="irriFile.html"> Irrigation</a><a href="tempFile.html"> Temperature</a></h1>\n'
+htmlTankText = ""
+htmlIrriText = ""
+htmlTempText = ""
+
 # Turn off GPIO warnings
 GPIO.setwarnings(False)
 time
 oneZeroValues = [0*x for x in range(500)]#list stores previous values of data from devices
 previousDataList = ['00000000' for x in range(500)]#list stores previous values of data from more analog style sensors
+managedDevices = []
 def validateData(data):
     if len(data) != 10 or data[0] != '1': #Data received not formatted correctly, write
         #to error log with poor data and timestamp.
@@ -46,6 +52,8 @@ def validateData(data):
         errorLog.close() #close error file
         return False
     elif type(getDevData(data)) is bool:
+		print("Device not found in device list")
+		print(type(getDevData(data)))
         errorLog = open('iot/errorLog.txt', 'a')
         time = datetime.datetime.now() #get time for use as timestamp
         errorLog.write(str(data) + " " + str(time) + "\n") #write poorly formatted data with timestap to file
@@ -57,7 +65,7 @@ def validateData(data):
 
 def getDevData(data):
     devData = ["numb", "type", "val1", "val2", "val3"]
-    devDataFile = open("iot/data.txt", "r")
+    devDataFile = open("iot/deviceData.txt", "r")
     fileLine = devDataFile.readlines()
     for n in fileLine:
         if data[1:4] == n[0:3]:#device found
@@ -70,25 +78,85 @@ def getDevData(data):
             return deviceData
     devDataFile.close()
     return False
+#go through list of devices, storing each device in managedDevices variable
+def checkPotentialDevices():
+	global managedDevices
+	devData = ["numb", "type", "val1", "val2", "val3"]
+    devDataFile = open("iot/deviceData.txt", "r")
+    fileLine = devDataFile.readlines()
+	for n in fileLine:
+        deviceData[0] = n[0:3] #devNum
+        deviceData[1] = n[4:8] #devType
+        deviceData[2] = n[9:13] #val1
+        deviceData[3] = n[14:18] #val2
+        deviceData[4] = n[19:23] #val3
+		managedDevices = managedDevices + n[0:8]
+    devDataFile.close()
+#should be run after checkPotentialDevices
+def createMenuText():
+	global htmlTankText
+    global htmlIrriText
+    global htmlTempText
+    global htmlMenuText
+    global managedDevices
+    tank = False
+    irri = False
+    temp = False
+    for n in managedDevices:
+        if n[4:8] == 'tank':
+            tank = True
+            htmlTankText = htmlTankText + '<a href="tankFile'+str(n[1:4])+'.html">Tank '+str(n[1:4])'</a>'
+        elif n[4:8] == 'irri':
+            irri = True
+            htmlIrriText = htmlIrriText + '<a href="irriFile'+str(n[1:4])+'.html">Irri '+str(n[1:4])'</a>'
+        elif n[4:8] == 'temp':
+            temp = True
+            htmlTempText = htmlTempText + '<a href="tempFile'+str(n[1:4])+'.html">Temp '+str(n[1:4])'</a>'
+    htmlMenuText = '<div class="navbar">\n<a href="index.html">Home</a>\n<div class="dropdown">\n<button class="dropbtn">Irrigation<i class="fa fa-caret-down"></i></button>\n<div class="dropdown-content">\n'
+    if tank:
+        htmlMenuText = htmlMenuText + '<div class = "dropdown">\n<button class="dropbtn">Tank<i class="fa fa-caret-down"></i></button>\n<div class="dropdown-content">\n'+htmlTankText+'</div></div>'
+    elif irri:
+        htmlMenuText = htmlMenuText + '<div class = "dropdown">\n<button class="dropbtn">Irri<i class="fa fa-caret-down"></i></button>\n<div class="dropdown-content">\n'+htmlIrriText+'</div></div>'
+    elif temp:
+        htmlMenuText = htmlMenuText + '<div class = "dropdown">\n<button class="dropbtn">Temp<i class="fa fa-caret-down"></i></button>\n<div class="dropdown-content">\n'+htmlTempText+'</div></div>'
+    htmlMenuText = htmlMenuText + '\n</div>'
 
-def writeData(data):
+def writeData(data):#write data to log file data.txt
     dataFile = open('iot/data.txt', 'a') # opens file for data to be written to
     time = datetime.datetime.now()
     dataFile.write(str(data[0:8])+ " " + str(time) + "\n")
     dataFile.close()
 
 def writeTank(data, devData):
-    tankFile = open("iot/tankFile.html", 'w')
-    tankFile.write('<!DOCTYPE html>\n<html>\n<body><h1><a href="tankFile.html">Tanks </a><a href="irriFile.html"> Irrigation</a></h1>\n')
+    global htmlMenuText
+    tankFile = open("/var/www/html/tankFile"+str(data[1:4])+".html", 'w')
+    htmlTop = open('Top.html', 'r')
+    htmlBottom = open('Bottom.html, 'r')
+	tankFile.write(htmlTop.read() + htmlMenuText + htmlBottom.read())
     tankFile.write('<div class="dataDisplay"><div style="width:50%; height:'+ str(int(data[4:8])/int(devData[2]) * 300) + 'px; border:1px solid black"></div><div style="width:50%; height:'+ str(3*(100 - int(data[4:8])/int(devData[2]) * 100)) + 'px; border:1px solid black; background-color: green;"></div>\n<h1>Tank ' + devData[0] + ' is at ' + str(100 - int(data[4:8])/int(devData[2]) * 100)+' %</h1>\n</div>\n')
-    tankFile.write("</body>\n</html>\n")
     tankFile.close()
+    htmlTop.close()
+    htmlBottom.close()
 
-def writeIrri(data, devData)
-    irriFile = open("iot/irriFile.html", "w")
-    irriFile.write('<!DOCTYPE html>\n<html>\n<body><h1><a href="tankFile.html">Tanks </a><a href="irriFile.html"> Irrigation</a></h1>\n')
-    irriFile.write("</body>\n</html>\n")
+def writeIrri(data, devData):
+    global htmlMenuText
+    irriFile = open("/var/www/html/irriFile"+str(data[1:4])+".html", "w")
+    htmlTop = open('Top.html', 'r')
+    htmlBottom = open('Bottom.html', 'r')
+    irriFile.write(htmlTop.read() + htmlMenuText + htmlBottom.read())
     irriFile.close()
+    htmlTop.close()
+    htmlBottom.close()
+
+def writeIndex():
+    gloabl htmlMenuText
+    htmlTop = open('Top.html', 'r')
+    htmlBottom = open('Bottom.html', 'r')
+    indexFile = open("/var/www/html/index.html", "w")
+    indexFile.write(htmlTop.read() + htmlMenuText + htmlBottom.read())
+    indexFile.close()
+    htmlTop.close()
+    htmlBottom.close()
 
 def processSerial(data):
     global oneZeroValues
@@ -99,15 +167,22 @@ def processSerial(data):
         prevData = previousDataList[int(data[1:4])]
         if prevData != data[0:8]: # value must have changed, updates need to occur
             writeData(data)
+			previousDataList[int(data[1:4])] = data[0:8]
             if devData[1] == 'tank':
                 writeTank(data, devData)
             elif devData[1] == "irri"
                 writeIrri(data, devData)
+				r=requests.post('https://maker.ifttt.com/trigger/binaryChange/with/key/jb2RMajdLlIxlszEeEsbqudfU6m_QWbGzghrDpaFQNc', params={"value1":str(data[0:4]), "value2":"No Water", "value3":"Water"})
+			elif devData[1] = "temp":
+				writeTemp(data, devData)
 
 
 try:
     print("    Ready")
     flag = 0
+    checkPotentialDevices()
+    createMenuText()
+    writeIndex()
     while flag==0:
         try:
             ser = serial.Serial('/dev/ttyACM0', 9600) #define serial port for communication with arduino
